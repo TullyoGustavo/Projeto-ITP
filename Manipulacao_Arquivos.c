47e218a3915981f9c4884be424bc19527244bf1d
Tullyo <tullyogustavo@hotmail.com>
2013-11-21 00:10:13 -0200

    Update Manipulacao_Arquivos.c

--- Manipulacao_Arquivos.c
+++ Manipulacao_Arquivos.c
@@ -1,28 +1,28 @@
 #include<stdio.h>
-#define MAX=500 
+#define MAX=500
 
-typedef enum { 
-        LIMPEZA, //0 
-        ALIMENTO_PERECIVEL, //1 
-        ALIMENTO_NAO_PERECIVEL, //2 
+typedef enum {
+        LIMPEZA, //0
+        ALIMENTO_PERECIVEL, //1
+        ALIMENTO_NAO_PERECIVEL, //2
         ELETRONICO //3
-} Categoria; 
+} Categoria;
 
-typedef struct { 
+typedef struct {
 
         int a;
         int m;
         int d;
-        
-}Data; 
 
-typedef struct{ 
-   
-        int c; //Corredor 
+}Data;
+
+typedef struct{
+
+        int c; //Corredor
         int p; //Prateleira
-        
+
 }Local;
-        
+
 typedef struct {
 
         Categoria cat;
@@ -32,60 +32,60 @@ typedef struct {
         char descricao[500];
         char nome[50];
         char fabricante[50];
-        Data validade; 
-        Local localizacao;
-        
-}Prod; 
+        Data validade;
+        Local posicao;
+
+}   Prod;
 
 
 int main() {
-        Prod estoque[MAX]={0};
-        int n, i, x, y;
-        
-        FILE *arquivo = fopen( "ESTOQUE.txt", "r" ); 
-        
+        Prod estoque[MAX];
+        int n, i, x;
+
+        FILE *arquivo = fopen( "ESTOQUE.txt", "r" );
+
         if( arquivo == NULL ){
                 printf( "Erro na abertura do arquivo" );
                 }
-                else { 
+                else {
                 fscanf( arquivo, "numero = %i\n", &n );
-                printf( "Existe %i PRODUTOS CADASTRADOS\n", n ); 
-                
+                printf( "Existe %i PRODUTOS CADASTRADOS\n", n );
+
                 for( i = 0 ; i < n; i++ ) {
-                
+
                         fscanf( arquivo," %i", &estoque[i].cat );
                         fscanf( arquivo," %i", &estoque[i].codigo);
                         fscanf( arquivo," %i", &estoque[i].preco);
-                        fscanf( arquivo," %i", &estoque[i].descricao); 
-                        fscanf( arquivo," %i", &estoque[i].nome); 
-                        fgets( estoque[i].descricao, 500, arquivo); 
+                        fscanf( arquivo," %i", &estoque[i].descricao);
+                        fscanf( arquivo," %i", &estoque[i].nome);
+                        fgets( estoque[i].descricao, 500, arquivo);
                         fgets( estoque[i].nome, 50, arquivo);
                         fgets( estoque[i].fabricante, 50, arquivo);
                         fscanf( arquivo," %i", &estoque[i].validade.a);
                         fscanf( arquivo," %i", &estoque[i].validade.m);
                         fscanf( arquivo," %i", &estoque[i].validade.d);
-                        fscanf( arquivo," %i", &estoque[i].posicao.c); 
-                        fscanf( arquivo," %i", &estoque[i].posicao.p); 
-                        
-                } 
-                        fclose( arquivo ); 
-                        } 
-                        
-                        
+                        fscanf( arquivo," %i", &estoque[i].posicao.c);
+                        fscanf( arquivo," %i", &estoque[i].posicao.p);
+
+                }
+                        fclose( arquivo );
+                        }
+
+
                 for( i = 0 ; i < n; i++ ) {
-                        fprintf( stdout,"Categoria %i", &estoque[i].cat );
-                        fprintf( stdout,"Codigo %i", &estoque[i].codigo); 
-                        fprintf( stdout,"Preco %.2f", &estoque[i].preco);
-                        fprintf( stdout,"Descricao %s", &estoque[i].descricao);
-                        fprintf( stdout,"Nome %s", &estoque[i].nome); 
-                        fprintf( stdout,"Fabricante %s" estoque[i].fabricante); 
-                        fprintf( stdout,"Ano %i", &estoque[i].validade.a); 
-                        fprintf( stdout,"Mes %i", &estoque[i].validade.m); 
-                        fprintf( stdout,"Dia %i", &estoque[i].validade.d);
-                        fprintf( stdout,"Corredor %i", &estoque[i].posicao.c); 
-                        fprintf( stdout,"Prateleira %i", &estoque[i].posicao.p); 
-                } 
-        } 
-        
-        return 0; 
+                        fprintf( stdout,"Categoria %i\n", &estoque[i].cat );
+                        fprintf( stdout,"Codigo %i\n", &estoque[i].codigo);
+                        fprintf( stdout,"Preco %.2f\n", &estoque[i].preco);
+                        fprintf( stdout,"Descricao %s\n", &estoque[i].descricao);
+                        fprintf( stdout,"Nome %s\n", &estoque[i].nome);
+                        fprintf( stdout,"Fabricante %s\n", estoque[i].fabricante);
+                        fprintf( stdout,"Ano %i\n", &estoque[i].validade.a);
+                        fprintf( stdout,"Mes %i\n", &estoque[i].validade.m);
+                        fprintf( stdout,"Dia %i\n", &estoque[i].validade.d);
+                        fprintf( stdout,"Corredor %i\n", &estoque[i].posicao.c);
+                        fprintf( stdout,"Prateleira %i\n", &estoque[i].posicao.p);
+                }
+
+
+        return 0;
 }
